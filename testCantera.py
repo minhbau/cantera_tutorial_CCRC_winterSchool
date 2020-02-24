0d7e0b274f4502b8e5a51d1556eb7a750bb7b6db
minhbau <minhbaubka@gmail.com>
2020-02-24 11:56:12 +0300

    add testCantera.py

---
+++ testCantera.py
@@ -0,0 +1,9 @@
+#! simple test to ensure that cantera importeds and runs properly
+ipython3
+import numpy as np
+import cantera as ct
+
+# check install path
+print(ct.__path__)
+# check cantera version
+print(ct.__version__)
