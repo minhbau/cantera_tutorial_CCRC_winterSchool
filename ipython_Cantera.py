aff8d8f6b67b1fac666e462e07b02ca444010cff
minhbau <minhbaubka@gmail.com>
2020-02-25 03:53:45 +0300

    add ipython_Cantera.py

--- ipython_Cantera.py
+++ ipython_Cantera.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# simple test to ensure that cantera importeds and runs properly
+# load cantera
 import cantera as ct
 
 # to work with numpy array
@@ -19,6 +19,9 @@ dir(cantera)
 
 # set the mechanism name
 gas = ct.Solution('gri30.xml')
+# the mechanism, gri30.xml
+# GRI-Mech 3.0 mechanism consists of 325 reactions that involve 53 species 
+# to model natural gas combustion.
 
 # get helps
 help(gas.species_index)
@@ -44,9 +47,8 @@ Tin = 1000.0 # K
 Pin = 40.0 # atm
 composition = 'CH4:1.0, O2:2,N2:7.52' 
 
-# set up the reactor
+# set up a state
 gas.TPX = Tin, Pin*ct.one_atm, composition
-
 
 print(gas.X) # returns an array of moles fractions
 print(gas.Y) # returns an array of mass fractions
@@ -61,6 +63,11 @@ print(gas['CH4'].partial_molar_cp)
 # ====================================================================
 #! select reactor: UV fixed specific internal energy and specific volume
 # ====================================================================
+# intial condition:
+Tin = 1000.0 # K
+Pin = 40.0 # atm
+composition = 'CH4:1.0, O2:2,N2:7.52' 
+
 gas.TPX = Tin, Pin*ct.one_atm, composition
 gas.equilibrate('UV')
 Teq = gas.T # get equilibrium (adiabatic) temperature
@@ -70,6 +77,11 @@ print(Teq)
 # ====================================================================
 #! select reactor: HP fixed specific enthalpy and pressure 
 # ====================================================================
+# intial condition:
+Tin = 1000.0 # K
+Pin = 40.0 # atm
+composition = 'CH4:1.0, O2:2,N2:7.52' 
+
 gas.TPX = Tin, Pin*ct.one_atm, composition
 gas.equilibrate('HP')
 print(gas.T)
@@ -77,29 +89,35 @@ print(gas.T)
 
 
 # ====================================================================
-# find the corresponding adibatic temperature with the UV reactor
-# by using a loop over a  range of equivalence ratio, phi
+#! find the indices of just those reactions which convert CO into CO2:
 # ====================================================================
-npoints = 20
-phi = np.linspace(0.1, 2.0, npoints)
-T_ad = np.zeros(npoints)
-for i in range(npoints):
-    gas.TP = 300, ct.one_atm
-    gas.set_equivalence_ratio(phi[i], 'CH4', 'O2:1, N2:3.76')
-    gas.equilibrate('UV')
-    T_ad[i] = gas.T
-    print(phi[i], T_ad[i])
+gas = ct.Solution('gri30.xml')
+II = [i for i,r in enumerate(gas.reactions())
+    if 'CO' in r.reactants and 'CO2' in r.products]
+for i in II:
+    print(i, gas.reaction(i).equation)
+gas.net_rates_of_progress[II]
 
-print(T_ad)
+# ===========================================================================
 
-# save the result to a file
-np.savetxt('save_phi_Tad.dat', np.transpose([phi, T_ad]), delimiter=' ', fmt='%.2f %12.5e')
 
-# plotting
-plt.plot(phi,T_ad)
-plt.xlabel('phi')
-plt.ylabel('T_ad (K)')
-# show the plot
-plt.show()
-# ====================================================================
+# ===========================================================================
+#! Equation of state (EoS):
+# ===========================================================================
+# P =  m/V *RT/MW = rho*T*R/MW = rho*T*R_specific
+# Where P in Pa=J/m^3, m in kg;  V m^3, T (K),
+# ideal, or universal gas constant R = 8.314 J/(K·mol), 
+
+# intial condition:
+Tin = 1000.0 # K
+Pin = 40.0 # atm
+composition = 'CH4:1.0, O2:2,N2:7.52' 
+
+# set up a state
+gas.TPX = Tin, Pin*ct.one_atm, composition
+print(P/ct.one_atm,ct.gas_constant,gas.T,gas.density,gas.mean_molecular_weight)
+
+# compute P by using EoS
+P = ct.gas_constant*gas.density*gas.T/gas.mean_molecular_weight
+print(P/ct.one_atm)
 
